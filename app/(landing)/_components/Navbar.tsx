fb2116c82ee48e97d26c468ed342c89c7e594145
adzibilal <adzibilal02@gmail.com>
2024-02-01 18:21:45 +0700

    feat: menu responsive

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -1,14 +1,25 @@
+'use client'
 import { navbarLinks } from '@/constants/navbarLinks'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import {BiMenu} from 'react-icons/bi'
-
+import React, { useState } from 'react'
+import { BiMenu } from 'react-icons/bi'
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
+    const handleClickMenu = () => {
+        if (showMenu) {
+            setShowMenu(false)
+        }
+    }
+
     return (
         <div className='bg-white shadow-lg sticky top-0 z-50'>
-            <div className='max-con py-5 flex justify-between items-center'>
+            <div className='max-con py-5 flex justify-between items-center relative'>
                 <Image
                     src='/images/logo.png'
                     width={106}
@@ -16,18 +27,28 @@ const Navbar = () => {
                     alt='Logo Forkeycreative'
                 />
 
-                <div className='flex items-center gap-4 max-md:hidden'>
+                <div
+                    className={`flex items-center gap-4 max-md:absolute max-md:bg-white max-md:top-[100%] max-md:left-0 max-md:w-full overflow-hidden max-md:flex-col max-md:py-5 transition-all duration-200 ease-in-out ${
+                        showMenu
+                            ? 'max-md:h-max'
+                            : 'max-md:h-0 max-md:!p-0'
+                    } `}>
                     {navbarLinks.map((link, index) => (
                         <Link
                             href={link.path}
+                            onClick={handleClickMenu}
                             className='cursor-pointer'
                             key={index}>
-                            <div className='px-3 py-1 border-2 border-transparent hover:border-b-blue-primary transition-all duration-150 ease-in-out'>{link.label}</div>
+                            <div className='px-3 py-1 border-2 border-transparent hover:border-b-blue-primary transition-all duration-150 ease-in-out'>
+                                {link.label}
+                            </div>
                         </Link>
                     ))}
                 </div>
 
-                <div className="hidden max-md:block text-4xl cursor-pointer">
+                <div
+                    onClick={toggleMenu}
+                    className='hidden max-md:block text-4xl cursor-pointer'>
                     <BiMenu />
                 </div>
             </div>
