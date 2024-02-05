310912f255480528d7ac024fbe4b6feee3550752
adzibilal <adzibilal02@gmail.com>
2024-02-05 09:42:31 +0700

    feat: init fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast';
+import FacebookPixel from './_components/FacebookPixel'
 
 const motserrat = Montserrat({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
             <body className={motserrat.className}>
                 {children}
                 <Analytics />
+                <FacebookPixel />
                 <Toaster />
             </body>
         </html>
