bf3684651fecff91b19f8326d7373db164c84cc8
adzibilal <adzibilal02@gmail.com>
2024-02-05 13:34:26 +0700

    feat: add meta image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
 import FacebookPixel from './_components/FacebookPixel'
+import Head from 'next/head'
 
 const motserrat = Montserrat({ subsets: ['latin'] })
 
@@ -20,6 +21,14 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={motserrat.className}>
+                <Head>
+                    <meta
+                        property='og:image'
+                        content='/images/meta-img.png'
+                    />
+                    <meta property='og:image:width' content='1900' />
+                    <meta property='og:image:height' content='950' />
+                </Head>
                 {children}
                 <Analytics />
                 <FacebookPixel />
