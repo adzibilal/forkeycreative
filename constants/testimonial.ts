e350165f605ebd00d96af561f7e353f9f1ce94d6
adzibilal <adzibilal02@gmail.com>
2024-02-20 09:56:01 +0700

    feat: add new testimonial

--- constants/testimonial.ts
+++ constants/testimonial.ts
@@ -16,5 +16,60 @@ export const testimonials = [
         job: 'Bussines Owner',
         comment:
             'Semenjak kerjasama sama forkey, bisnis saya berkembang cukup pesat. Berawal dari kerjasama konten, hingga akhirnya semua masalah digital matketing dihandle oleh tim forkey. Makasih banyak forkey!'
+    },
+    ,
+    {
+        name: 'Violita',
+        job: 'Business Owner',
+        comment:
+            'Susah berkata-kata kak, pokonya aku cocok nih sama tim forkey.. dengan harga terjangkau bisa dapat konten video sebagus & seprofesional itu. Sangat membantu untuk UMKM kaya aku. Sesuai bgt sama request yg aku mau pula plus bisa revisian juga. Proses dari kirim produk - shooting - finishing nya juga gak terlalu lama, oke lah untuk waktunya juga. Sukses terus ya tim Forkey Creative.'
+    },
+    {
+        name: 'Harlan Herdiansyah',
+        job: 'Business Owner',
+        comment:
+            'Overall oke banget. Mungkin yg perlu di perbaiki hal² kecil aja seperti kecepatan edit dan ketelitian, Data yang di berikan mungkin baik nya di drive, rapi sesuai penamaan article product nya. yang lain nya saya oke bgt. Insyaallah kerjasama nya bisa berkelanjutan, Saya ada 3 brand, dan semuanya mungkin akan gunakan jasa nya Forkey Creative.'
+    },
+    {
+        name: 'Muhammad rafi A',
+        job: 'Business Owner',
+        comment:
+            'Perfect banget deh pokoknya, pengerjaannya cepat, dan freelancer membantu client dengan senang hati. Goodjob mas, nanti saya akan order lagi.'
+    },
+    {
+        name: 'Yasa E',
+        job: 'Business Owner',
+        comment:
+            'Seperti biasa, bisa diandalkan. Kecepatan pengerjaan cepat banget, sesuai ekspektasi dan sesuai brief yang diberikan oleh client. Good.'
+    },
+    {
+        name: 'Krisnha Kanaya K',
+        job: 'Business Owner',
+        comment:
+            'Mantap pol.'
+    },
+    {
+        name: 'Unknown',
+        job: 'Business Owner',
+        comment:
+            'Recommended banget. Selalu cepat pengerjaannya dan suka banget sama hasilnya. Good.'
+    },
+    {
+        name: 'Erwin C',
+        job: 'Business Owner',
+        comment:
+            'Bagus livenya, recommended. sesuai, next time order lagi, thanks.'
+    },
+    {
+        name: 'Riza S',
+        job: 'Business Owner',
+        comment:
+            'Bagus, recommended.'
+    },
+    {
+        name: 'BogesCreative A.',
+        job: 'Business Owner',
+        comment:
+            'Sudah langganan, selalu memuaskan konten-kontennya. Terima kasih.'
     }
 ]
